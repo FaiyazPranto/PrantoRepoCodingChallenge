7800e290a0ed5adf5b0cfb434d64f201ed9b53bd
altaf <altaf@DESKTOP-2FG78DV.hitronhub.home>
2022-07-22 15:00:09 -0600

    Added my label.

--- src/application/Main.java
+++ src/application/Main.java
@@ -14,6 +14,7 @@ public class Main extends Application {
 			VBox root = new VBox();
 			root.getChildren().add(new Label("Farhina's label"));
 			root.getChildren().add(new Label("Farhina's branch"));
+			root.getChildren().add(new Label("Pranto's branch"));
 			Scene scene = new Scene(root,400,400);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Pranto's project");
@@ -22,9 +23,6 @@ public class Main extends Application {
 
 			e.printStackTrace(); 
 		}
-
-			e.printStackTrace();
-		} 
 
 	}
 	
